f0a902424dba482272fe786d2c773b6d19d7249a
isabelchua <isabelchua86@gmail.com>
2020-11-14 00:30:47 -0800

    save setup

--- src/reducers/ListReducer.js
+++ src/reducers/ListReducer.js
@@ -1,5 +1,29 @@
+import { initial } from "lodash";
 import { v4 } from "uuid";
 
-const ListReducer = () => {};
+const item = {
+	id: v4(),
+	name: "Wake up"
+};
+
+const item2 = {
+	id: v4(),
+	name: "Clean the house"
+};
+
+const initialState = [
+	{
+		id: 1,
+		name: "gym",
+		duration: "1 hour"
+	},
+
+	{
+		id: 1,
+		name: "John Doe"
+	}
+];
+
+const ListReducer = (state = initialState, action) => {};
 
 export default ListReducer;
